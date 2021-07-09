15e7b6c3f29137524716b66417c0f4b940d6474a
tery chinz <chinz0020@gmail.com>
2021-07-10 01:00:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { useReducer, useEffect } from 'react'
+import axios from 'axios'
+import logo from './logo.svg'
+import './App.css'
+const fetchCat = () => axios.get('https://aws.random.cat/meow')
+const initialState = {
+  isFetching: false,
+  cat: {},
+  count: 0
+}
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case 'FETCH_CAT_PENDING':
+      return {
+        ...state,
+        isFetching: true
+      }
+    case 'FETCH_CAT_SUCCESS':
+      return {
+        ...state,
+        isFetching: false,
+        cat: payload
+      }
+    case 'COUNTER_CLICK':
+      return {
+        ...state,
+        isFetching: false,
+        count: payload
+      }
+      case 'SUB_CLICK':
+        return {
+          ...state,
+          isFetching: false,
+          count: payload <= 0 ? 0 : payload
+        }
+    default:
+      return state
+  }
+}
+const App = () => {
+  // const [state, dispatch] = useReducer(reducer, initialState)
+  const [{ cat, isFetching, count }, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_CAT_PENDING'
+    })
+    fetchCat().then(response => {
+      dispatch({
+        type: 'FETCH_CAT_SUCCESS',
+        payload: response.data
+      })
+    })
+  }, [])
+  if (isFetching) {
+    return <p>Loading....</p>
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>You click {count} times</p>
+        <button
+          style={{
+            padding: '8px 16px',
+            borderRadius: 4,
+            fontSize: '1.25rem'
+          }}
+          onClick={() => {
+            dispatch({
+              type: 'COUNTER_CLICK',
+              payload: count + 1
+            })
+          }}
+        >
+          Click me
+        </button>
+        <button
+          style={{
+            padding: '8px 16px',
+            borderRadius: 4,
+            fontSize: '1.25rem'
+          }}
+          onClick={() => {
+            dispatch({
+              type: 'SUB_CLICK',
+              payload: count - 1
+            })
+          }}
+        >
+          Click sub me
+        </button>
+        <p>
+          <img src={cat.file} alt="Cat" width="256" />
+        </p>
+      </header>
+    </div>
+  )
+}
+export default App
